3af833e8658e48f603d32a3fec793c2e97ff138e
Uchechukwu Ugwuanyi <denisu29@gmail.com>
2020-12-28 10:05:35 +0100

    modified behaviour of some buttons when clicked

--- ixpn_dashboard/static/ajaxFormSubmit.js
+++ ixpn_dashboard/static/ajaxFormSubmit.js
@@ -6,6 +6,8 @@ $('.reset-button').on('click', function(){
     $("select").each(function() { this.selectedIndex = 0 });
     $(".numberinput").val("");
     $(".dateinput").val("");
+    let url = $("#Url").attr("data-url");
+    window.location.href = url;
 });
 $(document).on('click', '.confirm-delete', function(){
     return confirm('Are you sure you want to delete this?');
